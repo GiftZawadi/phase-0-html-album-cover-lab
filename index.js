e57714b70b35d436d2ae30f4a04cd7426b2c6580
Your Full Name <gzomandi07@gmail.com>
2024-03-14 10:52:24 +0300

    make album cover

---
+++ index.js
@@ -0,0 +1,45 @@
+forloops
+for(let i = 1; i <=100; i++) {
+    console.log(i)
+  }
+  switch.js
+  let grade = "C"
+
+switch (grade) {
+  case "A":
+        console.log("Congrats")
+        break;
+  case "B":
+        console.log("There is room for improvement")
+        break;
+  case "C":
+        console.log("You should learn more")
+        break;
+  case "D":
+        console.log("See me at the office")
+        break;
+  case "E":
+        console.log("Just drop out")
+        break;
+  default:
+        console.log("You are a teacher but you don't know how to teach")
+        break
+    }
+    index.js
+    let grade = 50:
+
+if (grade >= 75 && grade <=100) {
+    console.log ("Student has an A")
+}
+  else if(grade >= 65 && grade < 75) {
+    console.log ("Student has a B")
+  }
+    else if(grade >=55 && grade < 65) {
+      console.log ("Student has a C")
+    }
+      else if(grade >=45 && grade < 55) {
+        console.log ("Student has a D")
+      }
+      else if(grade >=0 && grade < 45)
+    console.log("Student failed")
+}
